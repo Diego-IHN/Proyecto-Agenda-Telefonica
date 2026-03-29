e1620b2697e465a2c36782adec4b679849470403
Luis <veachairezluisangel@gmail.com>
2026-03-28 18:24:16 -0700

    Cambios añadidos: validaciones a los setters y añadidos algunos comentarios

--- equipo/Persona.java
+++ equipo/Persona.java
@@ -1,17 +1,24 @@
+/*
+ * Asignatura: POO (Programación Orientada a Objetos)
+ * Unidad 3: Proyecto en equipo
+ * Docente: María Lucía Barrón Estrada
+ * -- Integrantes: -- 
+ * Luis Angel Vea Chairez 25171325
+ * Diego Antonio López Olivas 25171090
+ * Didier Montoya Samaniego 25170896
+ */
 package equipo;
 //Clase Persona
-
-//TODO: Eliminar el main
 public class Persona {
 	//Atributos
-	protected String nombre, apellidos, fechaNacimiento;
+	protected String nombre, apellidos, alias;
 	protected char sexo;
 	
 	//Constructor
-	public Persona(String nombre, String apellidos, String fechaNacimiento, char sexo) {
+	public Persona(String nombre, String apellidos, String alias, char sexo) {
 		this.nombre = nombre;
 		this.apellidos = apellidos;
-		this.fechaNacimiento = fechaNacimiento;
+		this.alias = alias;
 		this.sexo = sexo;
 	}
 	
@@ -28,11 +35,11 @@ public class Persona {
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
-	public String getFechaNacimiento() {
-		return fechaNacimiento;
+	public String getAlias() {
+		return alias;
 	}
-	public void setFechaNacimiento(String fechaNacimiento) {
-		this.fechaNacimiento = fechaNacimiento;
+	public void setAlias(String alias) {
+		this.alias = alias;
 	}
 	public char getSexo() {
 		return sexo;
@@ -43,12 +50,12 @@ public class Persona {
 	
 	//Método toString
 	public String toString() {
-		return String.format("Nombre: %s %s | Sexo: %c | Fecha de nacimiento: %s ", nombre, apellidos, sexo, fechaNacimiento );
+		return String.format("Nombre: %s %s | Alias: %s | Sexo: %c ", nombre, apellidos, alias, sexo);
 	}
 	
 	//Main de prueba
 	public static void main(String[]args) {
-		Persona p1 = new Persona("Diego", "López Olivas", "09-04-2007", 'H');
+		Persona p1 = new Persona("Diego", "López Olivas", "Ego", 'H');
 		System.out.println(p1);
 	}
 }
--- equipo/Telefono.java
+++ equipo/Telefono.java
@@ -1,47 +1,60 @@
+/*
+ * Asignatura: POO (Programación Orientada a Objetos)
+ * Unidad 3: Proyecto en equipo
+ * Docente: María Lucía Barrón Estrada
+ * -- Integrantes: -- 
+ * Luis Angel Vea Chairez 25171325
+ * Diego Antonio López Olivas 25171090
+ * Didier Montoya Samaniego 25170896
+ */
 package equipo;
 //Clase Teléfono
-
-//TODO: Eliminar el main
 public class Telefono {
 	//Atributos
-	protected char tipo;
-	protected String prefijo, numTel;
+	private char tipo;
+	private String prefijo, numTel;
 	
-	//Constructor
+	//Constructor, reutiliza los métodos setters para simplificar código
 	public Telefono(char tipo, String prefijo, String numTel) {
-		this.tipo = tipo;
-		this.prefijo = (prefijo == null) ? "XX" : prefijo;
-		this.numTel = (numTel == null) ? "Sin número" :numTel;
+		setTipo(tipo);
+		setPrefijo(prefijo);
+		setNumTel(numTel);
 	}
 	
 	//Getter y setters
 	public char getTipo() {
 		return tipo;
 	}
+	//Setter con validación de tipo de teléfono móvil (m) y fijo (f)
 	public void setTipo(char tipo) {
-		this.tipo = tipo;
+		if (tipo == 'm' || tipo == 'f') { this.tipo = tipo;}
+		else {
+			this.tipo = 'X';
+		}
 	}
 	public String getPrefijo() {
 		return prefijo;
 	}
+	//Setter con validación de prefijo (valida que no esté vacío y que contenga únicamente dos dígitos)
 	public void setPrefijo(String prefijo) {
-		this.prefijo = prefijo;
+		if (prefijo != null && prefijo.matches("\\d{2}")) { this.prefijo = prefijo; }
+		else {
+			this.prefijo = "XX";
+		}
 	}
 	public String getNumTel() {
 		return numTel;
 	}
+	//Setter con validación del número de teléfono (valida que no esté vacío y que contenga única y exclusivamente 10 dígitos)
 	public void setNumTel(String numTel) {
-		this.numTel = numTel;
+		if (numTel != null && numTel.matches("\\d{10}")) { this.numTel = numTel;}
+		else {
+			this.numTel = "0000000000";
+		}
 	}
 	
 	//Método toString
 	public String toString() {
 		return String.format("[%c](+%s)%s", tipo, prefijo, numTel);
-	}
-	
-	//Main de prueba
-	public static void main(String[]args) {
-		Telefono t1 = new Telefono('o',"52","6721221165");
-		System.out.println(t1);
 	}
 }
